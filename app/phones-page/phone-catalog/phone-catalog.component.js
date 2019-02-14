4204a3916ab87e78dbc31dd2edf6270505ad10c3
sfs-inep <igornepipenko@gmail.com>
2019-02-14 22:32:40 +0200

    Feature: custom events [ref:no-ref]

--- app/phones-page/phone-catalog/phone-catalog.component.js
+++ app/phones-page/phone-catalog/phone-catalog.component.js
@@ -1,20 +1,24 @@
 import { BaseComponent } from '../../common/components/base/base.component.js';
 
 export class PhonesCatalogComponent extends BaseComponent {
-  constructor({ element, phones, onPhoneSelect }) {
+  constructor({ element, phones, onPhoneSelect, onAdd }) {
     super({ element });
     this.phones = phones;
-    this.onPhoneSelect = onPhoneSelect;
+    // this.onPhoneSelect = onPhoneSelect;
+    // this.onAdd = onAdd;
     this._render();
-    this._element.addEventListener('click', this._handleClick.bind(this))
-  }
 
-  _handleClick({ target }) {
-    const liElement = target.closest('.thumbnail');
-    if (!liElement) {
-      return;
-    }
-    this.onPhoneSelect(liElement.dataset.id);
+    this.on('click', '.phone-link', (event)=>{
+      const liElement = event.target.closest('.thumbnail');
+      // this.onPhoneSelect(liElement.dataset.id);
+      this.emit('phone-select', liElement.dataset.id)
+    });
+
+    this.on('click', '.add-to-cart', (event)=>{
+      const liElement = event.target.closest('.thumbnail');
+     // this.onAdd(liElement.dataset.id);
+      this.emit('add', liElement.dataset.id)
+    });
   }
 
   _render() {
@@ -22,15 +26,15 @@ export class PhonesCatalogComponent extends BaseComponent {
           <ul class="phones">
           ${this.phones.reduce((html, phone) => {
       return `${html}     <li class="thumbnail" data-id=${phone.id}>
-          <a href="#!/phones/${phone.id}" class="thumb">
+          <a href="#/phones/${phone.id}" class="thumb phone-link">
             <img alt=${phone.id} src=${`assets/${phone.imageUrl}`}>
           </a>
           <div class="phones__btn-buy-wrapper">
-            <a class="btn btn-success">
+            <a class="btn btn-success add-to-cart">
               Add
             </a>
           </div>
-          <a href="#!/phones/${phone.id}">${phone.name}</a>
+          <a href="#/phones/${phone.id}" class="phone-link">${phone.name}</a>
           <p>${phone.snippet}</p>
         </li>`
     }, '')}
--- app/phones-page/phones-page.component.js
+++ app/phones-page/phones-page.component.js
@@ -1,28 +1,63 @@
 import { PhonesCatalogComponent } from './phone-catalog/phone-catalog.component.js';
 import { OnePhoneViewComponent } from './one-phone-view/one-phone-view.component.js';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component.js';
 import { PhonesPageService } from './phones-page.service.js';
 
 export class PhonesPageComponent {
   constructor({ element }) {
     this.element = element;
     this._render();
-
     this._phoneService = new PhonesPageService();
+    this._initCatalog();
+    this._initOnePhoneView();
+    this._initCart()
+  }
 
+  _initCatalog() {
     this._phoneCatalog = new PhonesCatalogComponent({
       element: this.element.querySelector('#catalog'),
       phones: this._phoneService.getAllPhones(),
-      onPhoneSelect: (phoneId)=>{
+      // onPhoneSelect: (phoneId) => {
+      //   const phoneDetails = this._phoneService.getPhonesById(phoneId);
+      //   this._phoneCatalog.hide();
+      //   this._phoneViewer.show(phoneDetails);
+      // },
+      // onAdd: (phoneId) => {
+      //   console.log(phoneId);
+      //   //TODO add cart
+      //   this._shoppingCart.add(phoneId);
+      // }
+    });
+    this._phoneCatalog.subscribe('phone-select', (phoneId) => {
         const phoneDetails = this._phoneService.getPhonesById(phoneId);
         this._phoneCatalog.hide();
         this._phoneViewer.show(phoneDetails);
-      }
-    });
+      })
 
+    this._phoneCatalog.subscribe('add',(phoneId)=>{
+      this._shoppingCart.add(phoneId);
+    })
+
+  }
+
+  _initOnePhoneView() {
     this._phoneViewer = new OnePhoneViewComponent({
       element: this.element.querySelector('#item'),
     });
+    this._phoneViewer.subscribe('back',()=>{
+      this._phoneCatalog.show();
+      this._phoneViewer.hide();
+    });
 
+    this._phoneViewer.subscribe('add',(phoneId)=>{
+      this._shoppingCart.add(phoneId);
+    })
+  }
+
+  _initCart() {
+   this._shoppingCart = new ShoppingCartComponent({
+     element: document.querySelector('#shopping-cart')
+   })
   }
 
   _render() {
@@ -45,14 +80,8 @@ export class PhonesPageComponent {
         </p>
       </section>
 
-      <section>
-        <p>Shopping Cart</p>
-        <ul>
-          <li>Phone 1</li>
-          <li>Phone 2</li>
-          <li>Phone 3</li>
-        </ul>
-      </section>
+ <div id="shopping-cart"></div>
+
     </div>
 
     <!--Main content-->
